7f611938cf327b25275f07a4f47d941fcaa6331d
Bryan Hoang <bryan@distributive.network>
2023-04-23 02:42:03 -0400

    chore(release): fix an api rate lmit error in CI
    Refs: https://github.com/semantic-release/github/pull/487#issuecomment-1486298997

---
+++ .releaserc.cjs
@@ -0,0 +1,35 @@
+/**
+ * @type {import('semantic-release').Options}
+ */
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        releaseRules: [
+          {
+            release: 'patch',
+            type: 'docs',
+          },
+          {
+            release: 'patch',
+            type: 'refactor',
+          },
+        ],
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/git',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/github',
+      {
+        // Avoid an API rate limit error.
+        successComment: false,
+        failTitle: false,
+      },
+    ],
+  ],
+};
